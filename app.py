bc48e2c2bd810a445edcee13491fcc96f1cd061b
rickgoldstein12 <rickgoldstein12@gmail.com>
2023-02-08 17:33:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def query_records():
+    return jsonify({'X': '1'})
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+app.run(debug=True)
