91bace36dcf00522a4bbe345b07f12089756be6d
rickgoldstein12 <rickgoldstein12@gmail.com>
2023-02-08 18:09:50 -0800

    more paths

--- app.py
+++ app.py
@@ -7,11 +7,21 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+
+
 @app.route('/', methods=['GET'])
 def query_records():
     return jsonify({'X': '1'})
 
+@app.route('/', methods=['POST'])
+def query_records():
+    return jsonify({'X': '2'})
+
 @app.route('/')
+def query_records():
+    return 'Hello, World!'
+
+@app.route('/p1')
 def hello_world():
     return 'Hello, World!'
 
